9e085e6f1bb645554c64444a69a3d0c1b8ea267d
Peterson da Silva Caetano <peterson_dhon@hotmail.com>
2022-05-18 10:33:57 -0300

    end header

--- src/script.js
+++ src/script.js
@@ -1,5 +1,21 @@
-$(document).ready(finction(){
+$(document).ready(function(){
 
-  $('#')
+  $('#menu').click(function(){
+    $(this).toggleClass('fa-times');
+    $('.navbar').toggleClass('nav-toggle');
+  });
+
+  $(window).on('scroll load', function(){
+
+    $('#menu').removeClass('fa-times');
+    $('.navbar').removeClass('nav-toggle');
+
+    if( $(window).scrollTop() > 68){
+      $('header .header-2').addClass('header-active');
+    }else{
+      $('header .header-2').removeClass('');
+    }
+
+  });
 
 });
